5f4bfe763f8043c9794ff9a54f6a3d7d61e198a0
Lee <blackmonkey9256@gmail.com>
2018-12-04 16:46:43 +0900

    Getting location automatically and fetching weather

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_weather_app/Model/weather_model.dart';
 import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
+import 'package:location/location.dart';
+import 'package:flutter/services.dart';
 
 Future<WeatherModel> getWeather(String lat, String lang) async{
   final response = await http.get('https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lang&appid=c9edd2391e6003880ee605b01c874b45&units=metric');
@@ -27,6 +29,29 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Map<String, double> currentLocation = new Map();
+  StreamSubscription<Map<String, double>> locationSubscription;
+
+  Location location = new Location();
+  String error;
+
+
+  @override
+  void initState() {
+    super.initState();
+    //Default value
+    currentLocation['latitude'] = 0.0;
+    currentLocation['logitude'] = 0.0;
+
+    initPlatformState();
+
+    locationSubscription = location.onLocationChanged().listen((Map<String, double> result){
+      setState(() {
+        currentLocation = result;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     //This codes is to fetch API, convert to data model and display name of place on screen
@@ -39,7 +64,7 @@ class _MyAppState extends State<MyApp> {
           decoration: BoxDecoration(color: Colors.white),
           child: Center(
             child: FutureBuilder<WeatherModel>(
-              future: getWeather('37', '126'),
+              future: getWeather(currentLocation['latitude'].toString(), currentLocation['longitude'].toString()),
               builder: (context, snapshot){
                 if(snapshot.hasData){
                   WeatherModel model = snapshot.data;
@@ -58,7 +83,7 @@ class _MyAppState extends State<MyApp> {
                         style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),),
                     ],),//Row
 
-                    Text('${fm.format(new DateTime.fromMicrosecondsSinceEpoch((model.dt*1000), isUtc: true))}',
+                    Text('${fm.format(new DateTime.fromMillisecondsSinceEpoch((model.dt*1000), isUtc: false))}',
                       style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
 
                     Text('Wind(Speed/Deg) : ${model.wind.speed}/${model.wind.deg}',
@@ -70,10 +95,10 @@ class _MyAppState extends State<MyApp> {
                     Text('humidity : ${model.main.humidity}',
                       style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
 
-                    Text('Sunrise : ${fm_hour.format(new DateTime.fromMicrosecondsSinceEpoch((model.sys.sunrise*1000), isUtc: true))}',
+                    Text('Sunrise : ${fm_hour.format(new DateTime.fromMillisecondsSinceEpoch((model.sys.sunrise*1000), isUtc: false))}',
                       style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
 
-                    Text('Sunset : ${fm_hour.format(new DateTime.fromMicrosecondsSinceEpoch((model.sys.sunset*1000), isUtc: true))}',
+                    Text('Sunset : ${fm_hour.format(new DateTime.fromMillisecondsSinceEpoch((model.sys.sunset*1000), isUtc: false))}',
                       style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
 
                     Text('GeoCode : [${model.coord.lat}/${model.coord.lon}]',
@@ -91,5 +116,24 @@ class _MyAppState extends State<MyApp> {
         ),//Container
       ),//Scaffold
     );//MaterialApp
+  }
+
+  void initPlatformState() async {
+    Map<String, double> my_location;
+    try{
+      my_location = await location.getLocation();
+      error = '';
+    }on PlatformException catch(e){
+      if(e.code == 'PERMISSION_DENIED'){
+        error = 'Permission Denied';
+      }else if(e.code == 'PERMISSION_DENIED_NEVER_ASK'){
+        error = 'Permission Denied - Please ask the user to enable it from app settings';
+      }
+      my_location = null;
+    }
+
+    setState(() {
+      currentLocation = my_location;
+    });
   }
 }
